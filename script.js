c0836cc4106dd43dfcb7885760b9247921067022
Roger <rochaclarore@vcu.edu>
2025-02-11 22:13:24 -0500

    implement styles.css for piano

---
+++ script.js
@@ -0,0 +1,52 @@
+// Vars
+const WHITE_KEYS = ["z", "x", "c", "v", "b", "n", "m"];
+const BLACK_KEYS = ["s", "d", "g", "h", "j"];
+
+const whiteKeys = document.querySelectorAll(".key.white");
+const blackKeys = document.querySelectorAll(".key.black");
+
+//Select all keys
+const keys = document.querySelectorAll(".key");
+
+// --Listeners--//
+// Add an event listener to all keys
+keys.forEach((key) => {
+  key.addEventListener("click", () => playNote(key));
+});
+
+// --Handlers --//
+function playNote(key) {
+  // Get the note's letter value
+  const noteAudio = document.getElementById(key.dataset.note);
+  //reset the notes current time to 0
+  noteAudio.currentTime = 0;
+  // Play the note
+  noteAudio.play();
+  // add a class, active, for styling purposes
+  key.classList.add("active");
+
+  // listens for when sound ends and remove 'active' class
+  noteAudio.addEventListener("ended", () => {
+    key.classList.remove("active");
+  });
+}
+
+document.addEventListener("keydown", (e) => {
+  // Get the key that triggered the event
+  const key = e.key;
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+  const blackKeyIndex = BLACK_KEYS.indexOf(key);
+
+  // If the key is being held down, don't play the note again
+  if (e.repeat) {
+    return;
+  }
+  
+  if (whiteKeyIndex > -1) {
+    playNote(whiteKeys[whiteKeyIndex]);
+  }
+
+  if (blackKeyIndex > -1) {
+    playNote(blackKeys[blackKeyIndex]);
+  }
+});
